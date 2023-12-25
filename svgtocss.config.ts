052954cca2451a347ee46ad6dacf42a56d708c6c
hunghg255 <giahunghust@gmail.com>
2023-12-25 13:52:43 +0700

    chore: export icon json

--- svgtocss.config.ts
+++ svgtocss.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'csvgtocss';
 export default defineConfig({
   src: 'public/svgicon', // svg path
   dist: 'public/svgcss', // output path
-  prefix: 'icon', // font name
+  prefix: 'icon', // font name,
+  exportJson: true, // export json file
 });
